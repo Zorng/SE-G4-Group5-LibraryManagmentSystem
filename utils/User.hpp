37d20ad62064bf91b066fd852f9cc80c69512c64
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-11-27 20:30:12 +0700

    feat: user list + implement doubly linked list

--- utils/User.hpp
+++ utils/User.hpp
@@ -2,21 +2,73 @@
 #include <string>
 using namespace std;
 
-class User {
+class UserNode {
     private:
         string userName;
         string userId;
-        int userAge;
+        string userPassword;
+        string userBirthday;
+        UserNode *next;
+        UserNode *prev;
 
-        // Transaction transactionList*
-    public:
-        //create a constructor for objects of the class
+        UserNode( string userName, string userId, string userPassword, string userBirthday) {
+            this->userName = userName;
+            this->userId = userId;
+            this->userBirthday = userBirthday;
+            this->userPassword = userPassword;
+            next = nullptr;
+            prev = nullptr;
+        }
 
-        //write a fuction that let the user borrow a book
+        friend class UserList;
+};
 
-        //write a fucntion that let the user return the book
+class UserList {
+    private:
+        UserNode *head;
+        UserNode *tail;
 
-        //write a fucntion that let the users see their own transaction history
+    public: 
+        UserList() {
+            head = nullptr;
+            tail = nullptr;
+        }
 
-        //write a function that display the user information
+        void insertFront (string userName, string userId, string userPassword, string userBirthday) {
+            UserNode *newUser = new UserNode(userName, userId, userPassword, userBirthday);
+            if (head == nullptr) {
+                head = newUser;
+            } else {
+                newUser->next = head;
+                head->prev = newUser;
+                head = newUser;
+            }
+        }
+
+        void insertBack(string userName, string userId, string userPassword, string userBirthday) {
+            UserNode *newUser = new UserNode(userName, userId, userPassword, userBirthday);
+            if (head == nullptr) {
+                head = tail = newUser;
+            } else {
+                newUser->prev = tail;
+                tail->next = newUser;
+                tail = newUser;
+            }
+        }
+
+        void removeFront() {
+            if (head == nullptr) {
+                cout << "List is empty" << endl;
+                return;
+            }
+
+            UserNode *temp = head;
+            if (head == tail) {
+                head = tail = nullptr;
+            } else {
+                head = head->next;
+                head->prev = nullptr;
+            }
+            delete temp;
+        }
 };
