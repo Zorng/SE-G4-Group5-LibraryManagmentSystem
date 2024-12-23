81206dab3df57b79630198cd0b47b55665e1aad8
Zorng <ieangzorngl@gmail.com>
2024-12-23 08:30:09 +0700

    added search by title and search by author name

--- utils/Book.hpp
+++ utils/Book.hpp
@@ -229,17 +229,16 @@ class BookList {
         while(1) {
             clearScreen();
             lower = ((currentPage-1)) * itemPerPage;
-            cout << "========================================" << endl;
-            cout << "Press left arrow to go to previous page, right arrow to go to next page, q to exit" << endl;
-            cout << "Press right arrow to go to next page" << endl;
-            cout << "Press q to exit" << endl;
-            cout << "========================================" << endl;
-            cout << "Current page "<< currentPage << endl;
-            cout << "Total page "<< totalPage << endl;
+            cout << "===== View all book =====" << endl;
+            cout << "Page "<< currentPage << " of " << totalPage << endl;
             //cout << "Total nav "<< ++k << endl;
             cout << "Total titles: "<< bookMap.size() << endl;
-            cout << "========================================" << endl;
-            cout << "title\t" << "auth\t" << "publish date\t" << "avaialable" << endl;
+            cout << endl;
+            cout << setw(25) << left << "     Title" 
+                 << setw(20) << left << "     Author" 
+                 << setw(20) << left << "     Publish date" 
+                 << setw(11) << left << "     avaialable" 
+                 << endl;
            // cout << "lower "<< lower << endl;
             map_it = bookMap.begin();
             for(int i = 0; i < lower; i++) {
@@ -247,19 +246,30 @@ class BookList {
             }  // set initial display element
             if(currentPage != totalPage) {
                 for(int i = 0; i < 10; map_it++, i++){
-                    cout << map_it->first << ", ";
+                    for(int i = 0; i < 82; i++) cout << "-";
+                    cout<< endl;
+                    cout << "| " << setw(25) << left << map_it->first << "";
                     list_it = map_it->second.begin();
-                    cout << get<0>(*list_it) << ", " << get<1>(*list_it) << ", " << get<2>(*list_it);
+                    cout << setw(20) << left << get<0>(*list_it) << "" 
+                         << "     "  << setw(15) << left << get<1>(*list_it) << ""
+                         << "        " << setw(7) << left << get<2>(*list_it) << "|";
                     cout << endl;
                 }
             } else { // last page
                  for(int i = 0; map_it != bookMap.end(); map_it++){
-                    cout << map_it->first << ", ";
+                    for(int i = 0; i < 82; i++) cout << "-";
+                    cout << endl;
+                    cout << "| " << setw(25) << left << map_it->first << "";
                     list_it = map_it->second.begin();
-                    cout << get<0>(*list_it) << ", " << get<1>(*list_it) << ", " << get<2>(*list_it);
+                    cout << setw(20) << left << get<0>(*list_it) << "" 
+                         << "     "<< setw(15) << left << get<1>(*list_it) << ""
+                         << "        " << setw(7) << left << get<2>(*list_it) << "|";
                     cout << endl;
                 }
             }
+            cout << endl;
+            cout << "*********************************************************" << endl;
+            cout << "Navigation:  previous page '<-', next page '->', exit 'q'" << endl;
             navKey = readNav();
             if(navKey == "left") {
                 if(currentPage != 1) currentPage--;
@@ -269,6 +279,7 @@ class BookList {
                 break;
             }
         }
+    }
 
 
         //Update the availability of the book status
@@ -276,7 +287,7 @@ class BookList {
             BookNode* current = head;
             while(current != nullptr){
                 if(current->bookID == inputItemID){
-                    current->availability == false; //Mark it as unvailable
+                    current->availability = false; //Mark it as unvailable
                     cout << "Book ID " << inputItemID << " has been marked as unavailable." << endl;
                     return;
                 }
@@ -285,61 +296,240 @@ class BookList {
             cout << "Book ID " << inputItemID << " not found in the list." << endl;
         }
 
-        //Admin add book to booklist
-        void add(string inputID, string inputTitle, string inputAuthName, string inputPublishDate){
-            //availability set to true when add new book
-            insertBack(inputID, inputTitle, inputAuthName, inputPublishDate, true);
-            cout << "Book ID " << inputID << " has been added successfully." << endl;
-        }
+    //Admin add book to booklist
+    void add(string inputID, string inputTitle, string inputAuthName, string inputPublishDate){
+        //availability set to true when add new book
+        insertBack(inputID, inputTitle, inputAuthName, inputPublishDate, true);
+        cout << "Book ID " << inputID << " has been added successfully." << endl;
+    }
         
-        //Edit books
-        void edit(string inputID){
-            BookNode* current = head;
-            while(current != nullptr){
-                if(current->bookID == inputID){
-                    cout << "Editing Book ID: " << inputID << endl;
-                    cout << "Current Title: " << current->bookTitle << endl;
-                    cout << "Enter new Title: ";
-                    cin.ignore();
-                    getline(cin, current->bookTitle);
+    //Edit books
+    void edit(string inputID){
+        BookNode* current = head;
+        while(current != nullptr){
+            if(current->bookID == inputID){
+                cout << "Editing Book ID: " << inputID << endl;
+                cout << "Current Title: " << current->bookTitle << endl;
+                cout << "Enter new Title: ";
+                cin.ignore();
+                getline(cin, current->bookTitle);
 
-                    cout << "Current Author: " << current->bookAuthor << endl;
-                    cout << "Enter new Author: ";
-                    getline(cin, current->bookAuthor);
-            
-                    cout << "Current Publish Date: " << current->bookPbDate << endl;
-                    cout << "Enter new Publish Date (format MM-YYYY): ";
-                    getline(cin, current->bookPbDate);
+                cout << "Current Author: " << current->bookAuthor << endl;
+                cout << "Enter new Author: ";
+                getline(cin, current->bookAuthor);
+        
+                cout << "Current Publish Date: " << current->bookPbDate << endl;
+                cout << "Enter new Publish Date (format MM-YYYY): ";
+                getline(cin, current->bookPbDate);
 
-                    cout << "Book ID " << inputID << " has been updated successfully!" << endl;
-                    return;
-                }
-                current = current->next;
-            }
-            cout << "Book ID " << inputID << " does not exist." << endl;
-        }
-
-        //Remove book by admin
-        void remove(string inputID){
-            BookNode* current = head;
-            bool found = false;
-            while(current != nullptr){
-                if(current->bookID == inputID){
-                    found = true;
-                    break;
-                }
-                current = current->next;
-            }
-
-            if(!found){
-                cout << "Input ID does not exist." << endl;
+                cout << "Book ID " << inputID << " has been updated successfully!" << endl;
                 return;
             }
-
-            //If found
-            removeID(inputID);
-            cout << "Book ID " << inputID << "has been removed." << endl;
+            current = current->next;
         }
+        cout << "Book ID " << inputID << " does not exist." << endl;
+    }
+
+    //Remove book by admin
+    void remove(string inputID){
+        BookNode* current = head;
+        bool found = false;
+        while(current != nullptr){
+            if(current->bookID == inputID){
+                found = true;
+                break;
+            }
+            current = current->next;
+        }
+
+        if(!found){
+            cout << "Input ID does not exist." << endl;
+            return;
+        }
+
+        //If found
+        removeID(inputID);
+        cout << "Book ID " << inputID << "has been removed." << endl;
+    }
+    void searchByTitle(string inputName) {
+        map<string, list<tuple<string, string, int>>> bookMap;
+        map<string, list<tuple<string, string, int>>>::iterator map_it;
+        list<tuple<string, string, int>>:: iterator list_it;
+        //  <title, <auth, date, count>>
+        BookNode* curr = head;
+        int itemPerPage = 10;
+        int totalPage;
+        int totalItem;
+        int currentPage;
+        while(curr != nullptr) {
+            if(inputName == curr->bookTitle) {
+                if(bookMap.size() == 0) {
+                    bookMap[curr->bookTitle].push_back(make_tuple(curr->bookAuthor, curr->bookPbDate, 1));
+                } else {
+                    for(map_it = bookMap.begin(); map_it != bookMap.end(); ++map_it){
+                        if(map_it->first == curr->bookTitle){
+                            get<2>(*map_it->second.begin())++;
+                        } 
+                    }
+                    if (map_it == bookMap.end()) {
+                        bookMap[curr->bookTitle].push_back(make_tuple(curr->bookAuthor, curr->bookPbDate, 1));
+                    
+                    }
+                }
+            } 
+            curr = curr->next;
+        }
+        totalItem = bookMap.size();
+        totalPage = (totalItem/10) + 1;
+        currentPage = 1;
+        string navKey;
+        int lower = 0;
+        int k = 0;
+        while(1) {
+            clearScreen();
+            lower = ((currentPage-1)) * itemPerPage;
+            cout << "===== Search by Title =====" << endl;
+            cout << "Page "<< currentPage << " of " << totalPage << endl;
+            cout << "Search input: " << inputName << endl;
+            //cout << "Total nav "<< ++k << endl;
+            cout << "Total titles: "<< bookMap.size() << endl;
+            cout << endl;
+            cout << setw(25) << left << "     Title" 
+                 << setw(20) << left << "     Author" 
+                 << setw(20) << left << "     Publish date" 
+                 << setw(11) << left << "     avaialable" 
+                 << endl;
+           // cout << "lower "<< lower << endl;
+            map_it = bookMap.begin();
+            for(int i = 0; i < lower; i++) {
+                map_it++;
+            }  // set initial display element
+            if(currentPage != totalPage) {
+                for(int i = 0; i < 10; map_it++, i++){
+                    for(int i = 0; i < 82; i++) cout << "-";
+                    cout<< endl;
+                    cout << "| " << setw(25) << left << map_it->first << "";
+                    list_it = map_it->second.begin();
+                    cout << setw(20) << left << get<0>(*list_it) << "" 
+                         << "     "  << setw(15) << left << get<1>(*list_it) << ""
+                         << "        " << setw(7) << left << get<2>(*list_it) << "|";
+                    cout << endl;
+                }
+            } else { // last page
+                 for(int i = 0; map_it != bookMap.end(); map_it++){
+                    for(int i = 0; i < 82; i++) cout << "-";
+                    cout << endl;
+                    cout << "|" << setw(25) << left << map_it->first << "";
+                    list_it = map_it->second.begin();
+                    cout << setw(20) << left << get<0>(*list_it) << "" 
+                         << "     "<< setw(15) << left << get<1>(*list_it) << ""
+                         << "        " << setw(7) << left << get<2>(*list_it) << "|";
+                    cout << endl;
+                }
+            }
+            cout << endl;
+            cout << "*********************************************************" << endl;
+            cout << "Navigation:  previous page '<-', next page '->', exit 'q'" << endl;
+            navKey = readNav();
+            if(navKey == "left") {
+                if(currentPage != 1) currentPage--;
+            } else if (navKey == "right") {
+                if(currentPage < totalPage) currentPage++;
+            } else if (navKey == "exit") {
+                break;
+            }
+        
+        }
+    }
+    void searchByAuth(string inputName) {
+        map<string, list<tuple<string, string, int>>> bookMap;
+        map<string, list<tuple<string, string, int>>>::iterator map_it;
+        list<tuple<string, string, int>>:: iterator list_it;
+        //  <title, <auth, date, count>>
+        BookNode* curr = head;
+        int itemPerPage = 10;
+        int totalPage;
+        int totalItem;
+        int currentPage;
+        while(curr != nullptr) {
+            if(inputName == curr->bookAuthor) {
+                if(bookMap.size() == 0) {
+                    bookMap[curr->bookTitle].push_back(make_tuple(curr->bookAuthor, curr->bookPbDate, 1));
+                } else {
+                    for(map_it = bookMap.begin(); map_it != bookMap.end(); ++map_it){
+                        if(map_it->first == curr->bookTitle){
+                            get<2>(*map_it->second.begin())++;
+                        } 
+                    }
+                    if (map_it == bookMap.end()) {
+                        bookMap[curr->bookTitle].push_back(make_tuple(curr->bookAuthor, curr->bookPbDate, 1));
+                    
+                    }
+                }
+            } 
+            curr = curr->next;
+        }
+        totalItem = bookMap.size();
+        totalPage = (totalItem/10) + 1;
+        currentPage = 1;
+        string navKey;
+        int lower = 0;
+        int k = 0;
+        while(1) {
+            clearScreen();
+            lower = ((currentPage-1)) * itemPerPage;
+            cout << "===== Search by Author =====" << endl;
+            cout << "Page "<< currentPage << " of " << totalPage << endl;
+            cout << "Search input: " << inputName << endl;
+            //cout << "Total nav "<< ++k << endl;
+            cout << "Total titles: "<< bookMap.size() << endl;
+            cout << endl;
+            cout << setw(25) << left << "     Title" 
+                 << setw(20) << left << "     Author" 
+                 << setw(20) << left << "     Publish date" 
+                 << setw(11) << left << "     avaialable" 
+                 << endl;
+           // cout << "lower "<< lower << endl;
+            map_it = bookMap.begin();
+            for(int i = 0; i < lower; i++) {
+                map_it++;
+            }  // set initial display element
+            if(currentPage != totalPage) {
+                for(int i = 0; i < 10; map_it++, i++){
+                    for(int i = 0; i < 82; i++) cout << "-";
+                    cout<< endl;
+                    cout << "| " << setw(25) << left << map_it->first << "";
+                    list_it = map_it->second.begin();
+                    cout << setw(20) << left << get<0>(*list_it) << "" 
+                         << "     "  << setw(15) << left << get<1>(*list_it) << ""
+                         << "        " << setw(7) << left << get<2>(*list_it) << "|";
+                    cout << endl;
+                }
+            } else { // last page
+                 for(int i = 0; map_it != bookMap.end(); map_it++){
+                    for(int i = 0; i < 82; i++) cout << "-";
+                    cout << endl;
+                    cout << "| " << setw(25) << left << map_it->first << "";
+                    list_it = map_it->second.begin();
+                    cout << setw(20) << left << get<0>(*list_it) << "" 
+                         << "     "<< setw(15) << left << get<1>(*list_it) << ""
+                         << "        " << setw(7) << left << get<2>(*list_it) << "|";
+                    cout << endl;
+                }
+            }
+            cout << endl;
+            cout << "*********************************************************" << endl;
+            cout << "Navigation:  previous page '<-', next page '->', exit 'q'" << endl;
+            navKey = readNav();
+            if(navKey == "left") {
+                if(currentPage != 1) currentPage--;
+            } else if (navKey == "right") {
+                if(currentPage < totalPage) currentPage++;
+            } else if (navKey == "exit") {
+                break;
+            }
+        }
+    }
 };
 
 
